7829b61b64a141f03b01c64c6becf5396a66f1bc
zainuddin25 <zanuk12345>
2023-05-03 16:40:50 +0700

    fix port

--- src/index.js
+++ src/index.js
@@ -4,6 +4,6 @@ app.all('/', (req, res) => {
     console.log("Just got a request!")
     res.send('<h1>Hallo world</h1>')
 })
-app.listen(process.env.PORT || 3000, () => {
+app.listen(process.env.CYCLIC_URL || 3000, () => {
     console.log(`Berjalan`)
 })
